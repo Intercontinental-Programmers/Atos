ae09b422b954e9dee721d48b60caff508fe215a7
Jakub Dudycz <dudyczjakub@gmail.com>
2018-04-06 23:25:36 +0200

    Allowed cross origin requests

--- BACKEND/src/main/java/com/ip/rest/controllers/AuthController.java
+++ BACKEND/src/main/java/com/ip/rest/controllers/AuthController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -20,6 +17,7 @@ import static com.ip.services.ValidationException.errorMapFromBindingResult;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin
 public class AuthController {
 
     private AppUserService appUserService;
