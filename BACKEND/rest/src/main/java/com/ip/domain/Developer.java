b43e5191d100484fd9e07d6afe99acf29711fc3e
Jakub Dudycz <dudyczjakub@gmail.com>
2018-04-07 01:12:53 +0200

    Reworked project structure pt2

--- BACKEND/rest/src/main/java/com/ip/domain/Developer.java
+++ BACKEND/rest/src/main/java/com/ip/domain/Developer.java
@@ -1,12 +1,5 @@
 package com.ip.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 public class Developer {
