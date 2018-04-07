597cbd094538eb4919ee76e5ae80a32a830a1d26
Jakub Dudycz <dudyczjakub@gmail.com>
2018-04-07 09:08:10 +0200

    hujkurwa

--- BACKEND/rest/src/main/java/com/ip/security/filters/AuthorizationFilter.java
+++ BACKEND/rest/src/main/java/com/ip/security/filters/AuthorizationFilter.java
@@ -27,8 +27,6 @@ public class AuthorizationFilter extends BasicAuthenticationFilter{
 									HttpServletResponse res,
 									FilterChain chain) throws IOException, ServletException {
 
-		if (req.getMethod().equals("OPTIONS"))
-			chain.doFilter(req, res);
 
 		String token = req.getHeader(HEADER_STRING);
 
