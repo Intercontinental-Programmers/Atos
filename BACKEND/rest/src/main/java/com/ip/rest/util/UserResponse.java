e2f8eb1c7f7f9e2d67e87d75e6066403b19881c5
Jakub Dudycz <dudyczjakub@gmail.com>
2018-04-07 06:43:15 +0200

    asdasd

--- BACKEND/rest/src/main/java/com/ip/rest/util/UserResponse.java
+++ BACKEND/rest/src/main/java/com/ip/rest/util/UserResponse.java
@@ -4,25 +4,16 @@ import com.ip.domain.AppUser;
 
 public class UserResponse {
 
-    private String name;
-    private String surname;
+    private String companyName;
     private String username;
     private String email;
 
     public UserResponse(AppUser user) {
-        name = user.getName();
-        surname = user.getSurname();
+        companyName = user.getCompanyName();
         email = user.getEmail();
         username = user.getUsername();
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
 
     public String getEmail() {
         return email;
@@ -30,6 +21,10 @@ public class UserResponse {
 
     public String getUsername() {
         return username;
+    }
+
+    public String getCompanyName() {
+        return companyName;
     }
 }
 
